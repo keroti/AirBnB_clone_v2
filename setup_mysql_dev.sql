7d8e9d1fe7552a29b11276694fbc30b973f4be1e
salawekesa <wekesa9235@gmail.com>
2023-01-07 16:06:42 +0300

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,11 @@
 -- script that prepares a MySQL server for the project
-
 -- creating a new databasa if it doesnt exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
 -- creating user if it doesnt exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
 -- granting user all permissions;
 GRANT ALL PERMISSIONS ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
+FLUSH PRIVILEDGES; 
 -- GRANTING select permisions to user
 GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
-
--- flushing priviledges
 FLUSH PRIVILEDGES; 
